cf24165067d549b07684c12d208e95c7f29c27ac
heeler <jamie.sherman@gmail.com>
2020-09-08 16:17:43 -0700

    changed cli executable to take a filename argument

--- main.cpp
+++ main.cpp
@@ -4,12 +4,25 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <locale>
+#include <codecvt>
+#include <string>
 
 #include "_aicspylibczi/Reader.h"
 
 int
-main(void){
-    pylibczi::Reader czi(L"/Users/jamies/Data/s_1_t_10_c_3_z_1.czi");
+main(int argc, char *argv[]){
+    if(argc != 2){
+        std::cout << "This executable is intended for profiling. It requires the user provide the path to a large czi file." << std::endl;\
+        std::cout << "Example Usage: profile_me /Users/jamies/Data/s_1_t_10_c_3_z_1.czi" << std::endl;
+        std::cerr << "Program called with insufficient or excess arguments!" << std::endl;
+        exit(0);
+    }
+
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring wide = converter.from_bytes(argv[1]);
+
+    pylibczi::Reader czi( wide.c_str() ); // L"/Users/jamies/Data/s_1_t_10_c_3_z_1.czi");
 
     libCZI::CDimCoordinate dm = libCZI::CDimCoordinate{{libCZI::DimensionIndex::B, 0},
                                                        {libCZI::DimensionIndex::S, 0}};
